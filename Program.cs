83c14bf5c5fec0b4b219a128f67fcdb3224ec482
Giorgio <kingleoon8@gmail.com>
2023-04-17 11:19:00 +0200

    SASSO CARTA FORBICE migliorato

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
-﻿
+﻿using System.Text.RegularExpressions;
 class Program
 {
 	static void Main(string[] args)
 	{
         Console.WriteLine("Inserisci il tuo nome");
-		Giocatore g1 = new Giocatore(Console.ReadLine()); // creo un oggetto di tipo Giocatore con nome Mario
+		Giocatore g1 = new Giocatore(Console.ReadLine()); // creo un oggetto di tipo Giocatore con un nome da input
 		 Console.WriteLine("Inserisci il nome del tuo avversario");
-        Giocatore g2 = new Giocatore(Console.ReadLine()); // creo un oggetto di tipo Giocatore con nome Computer
+        Giocatore g2 = new Giocatore(Console.ReadLine()); // creo un oggetto di tipo Giocatore con un nome da input
 		Partita p1 = new Partita(g1, g2); // creo un oggetto di tipo Partita con i giocatori g1 e g2
 		p1.Gioca(); // gioco la partita
 		p1.StampaPunteggio(); // stampo il punteggio dei giocatori
 	}
 }
-
